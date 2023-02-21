98879cad940480d3cc98659305796c7b102ff658
jacek Byczkiewicz <jck53416@gmail.com>
2023-02-21 22:03:31 +0100

    Enable debug tools

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,8 +9,10 @@ if (APP_CONFIG.production) {
   enableProdMode();
 }
 
+// ToDo(Jacek): Remove debug tools before release
 platformBrowserDynamic()
   .bootstrapModule(AppModule, {
     preserveWhitespaces: false
   })
+  .then(module => enableDebugTools(module.injector.get(ApplicationRef).components[0]))
   .catch(err => console.error(err));
