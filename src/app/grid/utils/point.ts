0b989c19b7466ac164de98d4efcff4f6b69538e0
jacek Byczkiewicz <jck53416@gmail.com>
2023-03-30 21:32:34 +0200

    Implement custom scroll behavior for grid cell
    It allows to scroll past an abolute or sticky container covering the cell

--- src/app/grid/utils/point.ts
+++ src/app/grid/utils/point.ts
@@ -1,4 +1,11 @@
+export type Direction = 1 | -1 | 0;
+
 export interface Point {
   x: number;
   y: number;
+}
+
+export interface PlaneDirection extends Point {
+  x: Direction;
+  y: Direction;
 }
