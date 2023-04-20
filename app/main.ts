87c36702f5daefc31f86e1981135a02dd67102d1
MateuszSzalas <69388837+MateuszSzalas@users.noreply.github.com>
2023-04-20 09:15:53 +0200

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -120,26 +120,3 @@ function handleIPCEvents() {
 
   });
 }
-
-
-//function handleIPCEvents() {
-//  ipcMain.handle('openExcel', async (event, args) => {
-//    const result = await dialog.showOpenDialog(win, {
-//      title: 'Select a file',
-//      filters: [
-//        {
-//          name: 'Spreadsheets',
-//          extensions: ['xlsx', 'xls', 'xlsb'],
-//        },
-//      ],
-//    });
-//    /* result.filePaths is an array of selected files */
-//    if (result.filePaths.length == 0) {
-//      throw new Error('No file was selected!');
-//    }
-//
-//    const workbook = XLSX.readFile(result.filePaths[0]);
-//    const first_ws = workbook.Sheets[workbook.SheetNames[0]];
-//    return XLSX.utils.sheet_to_json(first_ws);
-//  });
-//}
