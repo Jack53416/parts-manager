1f7fb7ae4570f9856685c04ba08f72228cfad9c1
MateuszSzalas <69388837+MateuszSzalas@users.noreply.github.com>
2023-04-20 08:44:17 +0200

    Update part.ts

--- src/app/parts-editor/models/part.ts
+++ src/app/parts-editor/models/part.ts
@@ -12,7 +12,7 @@ export interface Part {
 export const MOCK_PRODUCTION_REPORT: Part[] = [
   {
     machine: 'SG-101',
-    name: 'DUPA',
+    name: 'Test',
     articleNo: '12023-123',
     tool: '219',
     totalPartsProduced: 120,
@@ -22,31 +22,31 @@ export const MOCK_PRODUCTION_REPORT: Part[] = [
 
   {
     machine: 'SG-102',
-    name: 'DUPA2',
+    name: 'Test2',
     articleNo: '1213123-1233',
     tool: 'tools123',
     totalPartsProduced: 150,
     scrapNo: 0,
-    id: '1;2'
+    id: '3;4'
   },
 
   {
     machine: 'SG-110',
-    name: 'DUPA23',
+    name: 'Test23',
     articleNo: '12353-123',
     tool: '213434',
     totalPartsProduced: 100,
     scrapNo: 50,
-    id: '1;2'
+    id: '5;1'
   },
 
   {
     machine: 'SG-11023',
-    name: 'DUP123A',
+    name: 'Test23A',
     articleNo: '1232023-123',
-    tool: 'exampleTool',
+    tool: 'exampleTool11',
     totalPartsProduced: 50,
     scrapNo: 50,
-    id: '1;2'
+    id: '1;5'
   },
 ];
